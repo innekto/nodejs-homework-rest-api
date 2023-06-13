d8cf606f03a8198611a4284523b942e27c6d166f
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-06-13 20:56:12 +0300

    config comments

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
+// імпортуємо модуль Express.js, який дозволяє створювати та налаштовувати веб-сервер.
 const express = require("express");
+// логування http-запитів
 const logger = require("morgan");
+// дозволяємо робити запити з інших доменів
 const cors = require("cors");
-
-// ============підключення до бази даних=====================//
-const mongoose = require("mongoose");
-
-const DB_HOST =
-  "mongodb+srv://innekto:nenadoNichegonety@cluster0.gesoorn.mongodb.net/contacts?retryWrites=true&w=majority";
-mongoose
-  .connect(DB_HOST)
-  .then(() => console.log("database connect success"))
-  .catch((error) => console.log(error));
-// ============підключення до бази даних=====================//
-
+// модуль який дозволяє завантажити значення змінних середовища з файлу .env
+require("dotenv").config();
+// імпотруємо маршрутизатор
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
-
+//  формат логування в залежності від середовища виконання.
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
+//  middleware  Morgan для логування HTTP-запитів з використанням визначеного формату.
 app.use(logger(formatsLogger));
+//  middleware CORS для обробки запитів з інших доменів.
 app.use(cors());
+//  middleware для обробки JSON-даних, надісланих у тілі запиту.
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
-
+//  middleware, який відповідає за обробку запитів, які не відповідають жодному маршруту.
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
-
+// middleware для обробки помилок.
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// ============підключення до бази даних=====================//
+const mongoose = require("mongoose");
+
+const { DB_HOST, PORT } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(PORT))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+// ============підключення до бази даних=====================//
