bab0ec840eacbf42e6d9e27ecfabd76ce1824222
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-06-14 15:28:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const { DB_HOST, PORT } = process.env;
 
 mongoose
   .connect(DB_HOST)
-  .then(() => app.listen(PORT))
+  .then(() => {
+    app.listen(PORT);
+    console.log("Database connection successful");
+  })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
